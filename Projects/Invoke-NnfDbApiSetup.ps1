45c79a339f8be52330085c144b7273d27c7ac264
Eric Dieckman <eric.dieckman@wisc.edu>
2026-04-17 16:26:38 -0500

    Update NNFDB API csproj location

--- Projects/Invoke-NnfDbApiSetup.ps1
+++ Projects/Invoke-NnfDbApiSetup.ps1
@@ -12,7 +12,7 @@ function Invoke-NnfDbApiSetup {
         -WorkingDirectory $global:Project.NnfDbApi `
         -ClaudeSession 'NeverNotDB API' `
         -IncludeDevServerPane `
-        -DevServerCommand "Invoke-DotnetWatch -WorkingDirectory '$($global:Project.NnfDbApi)' -Project 'src\api\Api.csproj'"
+        -DevServerCommand "Invoke-DotnetWatch -WorkingDirectory '$($global:Project.NnfDbApi)' -Project 'src\api\NnfDb.Api.csproj'"
 }
 
 New-Alias -Name Setup-NnfDbApi -Value Invoke-NnfDbApiSetup -Force
